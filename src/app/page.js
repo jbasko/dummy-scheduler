758a9ef129f113cf17192724b556b073dbd6cb6a
Jazeps Basko <jazeps.basko@gmail.com>
2023-07-13 00:47:32 +0300

    First steps with calendar

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,24 @@
-import {Container} from "@mui/material";
+"use client"
+import React, {useState} from "react"
+import {Container} from "@mui/material"
+import Scheduler from "@/components/scheduler"
+
+import {LocalizationProvider} from "@mui/x-date-pickers"
+import {AdapterMoment} from "@mui/x-date-pickers/AdapterMoment"
+
+import "moment/locale/lv"
 
 export default function Home() {
+    const [locale, setLocale] = useState("lv-LV")
+
     return (
-        <Container maxWidth={"lg"}>
-            <div className={"my-8"}>
-                <h1 className={"text-primary-500 text-4xl font-semibold"}>Hello, world!</h1>
-            </div>
-        </Container>
+        <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale={locale}>
+            <Container maxWidth={"lg"}>
+                <div className={"my-8"}>
+                    <h1 className={"text-primary-500 text-4xl font-semibold"}>Hello, world!</h1>
+                    <Scheduler/>
+                </div>
+            </Container>
+        </LocalizationProvider>
     )
 }
---
+++ src/components/scheduler.js
@@ -0,0 +1,100 @@
+import Calendar from "@event-calendar/core";
+import TimeGrid from "@event-calendar/time-grid";
+import Interaction from "@event-calendar/interaction";
+
+import React, {useEffect, useMemo, useRef, useState} from "react"
+
+import "@event-calendar/core/index.css"
+import {DatePicker} from "@mui/x-date-pickers";
+import {Dialog, DialogContent, DialogContentText, DialogTitle, TextField} from "@mui/material";
+
+
+const EventEditor = ({open, onClose, event}) => {
+    return (
+        <Dialog
+            open={open}
+            onClose={onClose}
+        >
+            <DialogTitle>Edit Event</DialogTitle>
+            <DialogContent>
+                <DialogContentText>You can edit the event details here.</DialogContentText>
+                <TextField label={"Start time"} value={event?.start}/>
+                <TextField
+                    multiline={true}
+                    rows={4}
+                />
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+const Scheduler = ({date}) => {
+    const ref = useRef()
+    const [rendered, setRendered] = useState(false)
+    const [eventEditorOpen, setEventEditorOpen] = useState(false)
+    const [editedEvent, setEditedEvent] = useState(null)
+
+    const calendar = useMemo(() => {
+        if (!ref.current) return
+
+        const cal = new Calendar({
+            target: ref.current,
+            props: {
+                plugins: [TimeGrid, Interaction],
+                options: {
+                    editable: true,
+                    selectable: true,
+                    view: "timeGridWeek",
+                    firstDay: 1,
+                    locale: "lv-LV",
+                    allDaySlot: false,
+                    // scrollTime: "08:00:00",
+                    slotMinTime: "06:00:00",
+                    slotMaxTime: "24:00:00",
+                    // allDayContent: "...",
+                    headerToolbar: {start: "", center: "", end: ""}, // no header toolbar
+                    events: [],
+                    // dateClick: (info) => {
+                    // },
+                    select: ({start, end, allDay}) => {
+                        const event = {start, end, allDay}
+                        cal.addEvent(event)
+                        setEditedEvent(event)
+                        setEventEditorOpen(true)
+                    },
+                    eventClick: ({event}) => {
+                        setEventEditorOpen(!eventEditorOpen)
+                    },
+                }
+            }
+        });
+
+        return cal
+
+    }, [rendered, ref])
+
+    useEffect(() => {
+        setRendered(true)
+        return () => {
+            calendar.destroy()
+        }
+    }, [])
+
+    return (
+        <div>
+            <DatePicker onChange={(value) => {
+                if (value.isValid()) {
+                    calendar.setOption("date", value.toDate())
+                }
+            }} />
+            <div ref={ref}/>
+            <EventEditor
+                event={editedEvent}
+                open={eventEditorOpen}
+                onClose={() => setEventEditorOpen(false)}
+            />
+        </div>
+    )
+}
+
+export default Scheduler
